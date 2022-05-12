229f5eb867b01234dbe21477eee5a8b7058ac88b
SimoneSampognaro <s281132@studenti.polito.it>
2022-05-12 19:17:58 +0200

    mod

--- script_estrazione_dati.py
+++ script_estrazione_dati.py
@@ -3,16 +3,6 @@ import os
 with open('./waves_and_wind_parameters_dati1.csv', 'r') as file:
  reader = csv.reader(file)
  dati = [(linea[4],linea[6]) for linea in reader]
- #for dato in dati:
-  #      print(dato)
- #for row in reader:
-    #   print(row)
-
-#['significant_wave_height','mean_wave_direction']
-
-#dati=[(linea[4],linea[6]) for linea in reader if linea[4]!="" and linea[6] != ""]
-#for riga in dati:
-   #    print(riga)
 
 file_path = './estrazione.csv'
 try:
@@ -22,7 +12,6 @@ except OSError as e:
 
 with open('estrazione.csv', 'w', newline='') as fileOUT:
      writer = csv.writer(fileOUT)
-    # writer.writerow(['significant_wave_height','mean_wave_direction'])
      for dato in dati:
        writer.writerow(dato)
    
