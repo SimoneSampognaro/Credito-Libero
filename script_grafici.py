444b17bc6b58277936e7588005b92bbd12f2967a
SimoneSampognaro <s281132@studenti.polito.it>
2022-05-12 19:17:24 +0200

    file grafici pronto

--- script_grafici.py
+++ script_grafici.py
@@ -1,6 +1,6 @@
 import seaborn
 import pandas
 import matplotlib.pyplot as plt
-csv = pandas.read_csv(r'C:\waves_and_wind_parameters_dati1.csv')
-res = seaborn.lineplot(x="Name", y="Age", data=csv)
+csv = pandas.read_csv(r'./estrazione.csv')
+res = seaborn.lineplot(x="significant_wave_height", y="mean_wave_direction", data=csv)
 plt.show()
