5ea9699c267a6d81b54bc5779e4652af493d6bbf
SimoneSampognaro <s281132@studenti.polito.it>
2022-05-12 18:53:34 +0200

    init

--- script_estrazione_dati.py
+++ script_estrazione_dati.py
@@ -1,5 +1,11 @@
 import csv
 with open('./waves_and_wind_parameters_dati1.csv', 'r') as file:
  reader = csv.reader(file, delimiter=";")
- for row in reader:
-        print(row)
+ #for row in reader:
+     #   print(row)
+
+['significant_wave_height','mean_wave_direction']
+
+dati=[(linea[4],linea[6]) for linea in reader if linea[4]!="" and linea[6] != ""]
+for riga in dati:
+       print(riga)
