88e47eb23c44b1181bab49fecd2357a0f4be38cc
SimoneSampognaro <s281132@studenti.polito.it>
2022-05-17 16:08:50 +0200

    chagne

--- script_estrazione_dati.py
+++ script_estrazione_dati.py
@@ -1,16 +1,16 @@
 import csv
 import os
-with open('./waves_and_wind_parameters_dati1.csv', 'r') as file:
+with open('./waves_and_wind_parameters_dati2.csv', 'r') as file:
  reader = csv.reader(file)
- dati = [(linea[4],linea[6]) for linea in reader]
+ dati = [(linea[4],linea[6]) for linea in reader]    # 4 e 6 per mare, 7 e 8 per vento
 
-file_path = './estrazione.csv'
+file_path = './estrazioneSea.csv'
 try:
     os.remove(file_path)
 except OSError as e:
     print("Error: %s : %s" % (file_path, e.strerror))
 
-with open('estrazione.csv', 'w', newline='') as fileOUT:
+with open('estrazioneSea.csv', 'w', newline='') as fileOUT:
      writer = csv.writer(fileOUT)
      for dato in dati:
        writer.writerow(dato)
