a6160c62b47e1113b2727dc259e85fac1dd2f188
SimoneSampognaro <s281132@studenti.polito.it>
2022-05-17 16:49:59 +0200

    vhh

--- script_estrazione_dati.py
+++ script_estrazione_dati.py
@@ -1,8 +1,8 @@
 import csv
 import os
-with open('./waves_and_wind_parameters_dati2.csv', 'r') as file:
+with open('./waves_and_wind_parameters_dati1.csv', 'r') as file:
  reader = csv.reader(file)
- dati = [(linea[4],linea[6]) for linea in reader]    # 4 e 6 per mare, 7 e 8 per vento
+ dati = [(linea[3],linea[4],linea[6]) for linea in reader]    # 3, 4 e 6 per mare, 7 e 8 per vento
 
 file_path = './estrazioneSea.csv'
 try:
@@ -10,8 +10,17 @@ try:
 except OSError as e:
     print("Error: %s : %s" % (file_path, e.strerror))
 
+
 with open('estrazioneSea.csv', 'w', newline='') as fileOUT:
      writer = csv.writer(fileOUT)
      for dato in dati:
+       print(dato)
+      # CF = 0.49*(int)dato[1]*(int)pow(dato[2],2)
+     #  a= float(dato[1])
+     #  b= float(dato[2])
+     #  CF = 0.49 * a * b * b
+     #  print(CF)
        writer.writerow(dato)
-   
+       #dato.append(0.49*dato[1]*pow(dato[2],2))
+
+#index,lat,long,time,significant_wave_height,energy_wave_period,mean_wave_direction
