f135e043e99fa7b2d390bfef84ab7723efcddc24
SimoneSampognaro <s281132@studenti.polito.it>
2022-05-18 22:33:36 +0200

    dati

--- powerMatrix.py
+++ powerMatrix.py
@@ -6,3 +6,7 @@ with open('./wec_matrix.csv', 'r') as file:
  for linea in reader:
    matrice.append(linea)
 
+with open('./input.csv', 'r') as file2:
+    reader = csv.reader(file2,delimiter=',')
+    dati = [(linea[3], float(linea[4]), float(linea[5])) for linea in reader]  # 3 tempo 4 altezza 5 periodo
+
