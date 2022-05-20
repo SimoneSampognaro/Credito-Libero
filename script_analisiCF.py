066079ed20f534b10bf9fdb8c0f9483566157dc5
SimoneSampognaro <s281132@studenti.polito.it>
2022-05-20 13:57:26 +0200

    sopra

--- script_analisiCF.py
+++ script_analisiCF.py
@@ -27,4 +27,38 @@ dati2.pop(0)
 CF_sotto=[]
 for linea in dati2:
       CF_sotto.append(linea[4])
-      print(linea[4])
+      #print(linea[4])
+
+somma = 0
+count = 0
+sommaVarianzaSopra= 0
+
+for dato in CF_sopra:
+    somma = somma + float(dato)
+    sommaVarianzaSopra = sommaVarianzaSopra + pow(float(dato),2)
+    count = count + 1
+
+mediaSopra = somma/count
+mediaQuadratoSopra =  sommaVarianzaSopra/count
+
+somma = 0
+count = 0
+sommaVarianzaSotto = 0
+
+for dato in CF_sotto:
+    somma = somma + float(dato)
+    sommaVarianzaSotto = sommaVarianzaSotto + pow(float(dato),2)
+    count = count + 1
+
+mediaSotto = somma/count
+mediaQuadratoSotto =  sommaVarianzaSotto/count
+
+varianzaSopra = mediaQuadratoSopra - pow(mediaSopra,2)
+varianzaSotto = mediaQuadratoSotto - pow(mediaSotto,2)
+
+print("Ecco le medie, sopra: ",mediaSopra," sotto: ",mediaSotto)
+
+
+print("Ecco le varianze, sopra: ",varianzaSopra," sotto: ",varianzaSotto)
+
+# Sopra è meglio !!
