cee82ca320174bb1159eb1abade20c3776efc7ce
SimoneSampognaro <s281132@studenti.polito.it>
2022-05-23 18:08:02 +0200

    tabellaCFpronto

--- outputSlide8.py
+++ outputSlide8.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import numpy as np
 
 
 tempo=[]
@@ -17,7 +18,6 @@ with open('./CF_sopra.csv', 'r') as file:
  for linea in reader:
     datiCFsopra = [(linea[4]) for linea in reader]
 
-#print(datiCFsopra) WEC
 
 datiCFsotto=[]
 with open('./CF_sotto.csv', 'r') as file2:
@@ -25,62 +25,65 @@ with open('./CF_sotto.csv', 'r') as file2:
  for linea in reader:
     datiCFsotto = [(linea[4]) for linea in reader]
 
-#print(datiCFsotto) WEC
 
 datiCFoffshoresopra=[]
 with open('./offshore sopra.csv', 'r') as file3:
  reader = csv.reader(file3,delimiter=';')
  for linea in reader:
-    datiCFoffshoresopra = [(linea[7]) for linea in reader]
-    #print(linea)
+    datiCFoffshoresopra = [(linea[7].replace(",",".")) for linea in reader]
 
-#for dato in datiCFoffshoresopra:
-# print(dato)
 
 datiCFoffshoresotto=[]
 with open('./offshore sotto.csv', 'r') as file4:
  reader = csv.reader(file4,delimiter=';')
  for linea in reader:
-    datiCFoffshoresotto = [(linea[7]) for linea in reader]
+    datiCFoffshoresotto = [(linea[7].replace(",",".")) for linea in reader]
 
 datiCFonshoresopra=[]
 with open('./onshore sopra.csv', 'r') as file4:
  reader = csv.reader(file4,delimiter=';')
  for linea in reader:
-    datiCFonshoresopra = [(linea[7]) for linea in reader]
+    datiCFonshoresopra = [(linea[7].replace(",",".")) for linea in reader]
 
 datiCFoonshoresotto=[]
 with open('./onshore sotto.csv', 'r') as file5:
  reader = csv.reader(file5,delimiter=';')
  for linea in reader:
-    datiCFoonshoresotto = [(linea[7]) for linea in reader]
+    datiCFoonshoresotto = [(linea[7].replace(",",".")) for linea in reader]
 
 datiCFsolareNORD=[]
 with open('./solare 1MW nord.csv', 'r') as file6:
  reader = csv.reader(file6,delimiter=';')
  for linea in reader:
-    datiCFsolareNORD = [(linea[7]) for linea in reader]
+    datiCFsolareNORD = [(linea[7].replace(",",".")) for linea in reader]
 
 datiCFsolareSUD=[]
 with open('./solare 1MW sud.csv', 'r') as file7:
  reader = csv.reader(file7,delimiter=';')
  for linea in reader:
-    datiCFsolareSUD = [(linea[7]) for linea in reader]
+    datiCFsolareSUD = [(linea[7].replace(",",".")) for linea in reader]
+
+consumo=[]
+with open('./carico elettrico.csv', 'r') as file8:
+ reader = csv.reader(file8,delimiter=';')
+ for linea in reader:
+    consumo = [(linea[2].replace(",",".")) for linea in reader]
 
 
 risultato=[]
-risultato.append(["Tempo, CF wec sopra, CF wec sotto, CF OFS sopra, CF OFS sotto, CF ONS sopra, CF ONS sotto, CF sun sopra, CF sun sotto"])
+risultato.append(["Tempo, CF wec sopra, CF wec sotto, CF OFS sopra, CF OFS sotto, CF ONS sopra, CF ONS sotto, CF sun sopra, CF sun sotto,consumo"])
 
 for i in range(0,8760):
     daAppendere=[]
     daAppendere.append(tempo[i])
-    daAppendere.append(datiCFsopra[i])
-    daAppendere.append(datiCFsotto[i])
-    daAppendere.append(datiCFoffshoresopra[i])
-    daAppendere.append(datiCFoffshoresotto[i])
-    daAppendere.append(datiCFonshoresopra[i])
-    daAppendere.append(datiCFsolareNORD[i])
-    daAppendere.append(datiCFsolareSUD[i])
+    daAppendere.append(float(datiCFsopra[i]))
+    daAppendere.append(float(datiCFsotto[i]))
+    daAppendere.append(float(datiCFoffshoresopra[i]))
+    daAppendere.append(float(datiCFoffshoresotto[i]))
+    daAppendere.append(float(datiCFonshoresopra[i]))
+    daAppendere.append(float(datiCFsolareNORD[i]))
+    daAppendere.append(float(datiCFsolareSUD[i]))
+    daAppendere.append(float(consumo[i]))
     risultato.append(daAppendere)
 
 
--- script_grafici.py
+++ script_grafici.py
@@ -4,3 +4,13 @@ import matplotlib.pyplot as plt
 csv = pandas.read_csv(r'./estrazione.csv')
 res = seaborn.lineplot(x="significant_wave_height", y="mean_wave_direction", data=csv)
 plt.show()
+
+ #daAppendere.append(tempo[i])
+ #   daAppendere.append(float(datiCFsopra[i]))
+  #  daAppendere.append(float(datiCFsotto[i]))
+   # daAppendere.append(float(datiCFoffshoresopra[i]))
+   # daAppendere.append(float(datiCFoffshoresotto[i]))
+    #daAppendere.append(float(datiCFonshoresopra[i]))
+    #daAppendere.append(float(datiCFsolareNORD[i]))
+    #daAppendere.append(float(datiCFsolareSUD[i]))
+    #daAppendere.append(float(consumo[i]))
